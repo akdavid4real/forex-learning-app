22af4b9fb49773147bcc8eebcf4b37dc8d717145
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-14 11:39:51 +0100

    feat: add offline status hook

---
+++ src/use-network-status.ts
@@ -0,0 +1,12 @@
+import NetInfo from '@react-native-community/netinfo';
+import { useEffect, useState } from 'react';
+
+export function useNetworkStatus() {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => NetInfo.addEventListener((state) => {
+    setIsOnline(Boolean(state.isConnected && state.isInternetReachable !== false));
+  }), []);
+
+  return isOnline;
+}
